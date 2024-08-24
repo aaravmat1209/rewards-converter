d2baad136ce49eab16acc0491e9b3a7c40083d91
aaravmat1209 <aaravmatalia129@gmail.com>
2024-08-23 21:08:55 -0700

    Final changes made

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
 public class RewardValue {
-    private double cash;
+    private double flightCash;
     private double miles;
 
     public RewardValue(double cashVal) {
-        this.cash = cashVal;
+        this.flightCash = cashVal;
         this.miles = cashVal / 0.0035;
     }
 
     public RewardValue(int mileVal) {
         this.miles = mileVal;
-        this.cash = mileVal * 0.0035;
+        this.flightCash = mileVal * 0.0035;
     }
 
     public double getCashValue() {
-        return cash;
+        return flightCash;
     }
 
     public double getMilesValue() {
